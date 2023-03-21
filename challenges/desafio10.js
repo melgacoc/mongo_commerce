4e7d525dab2b76fbb542b4eafa84925aa6e4dfd4
Cláudio <claudio19a@hotmail.com>
2023-03-21 18:18:10 -0300

    sells excluding big mac and mcchicken

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,4 @@
 db.produtos.find({
-    valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } } } },
+    valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 },
+    } } },
     { _id: 0, nome: 1 });
