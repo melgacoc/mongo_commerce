9bd2d6b07e049959564e2447a50c3d5c7ac7f9e5
Cláudio <claudio19a@hotmail.com>
2023-03-22 10:30:34 -0300

    req 24

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({}, { $push: { tags: {
     $each: ["combo", "tasty"], $sort: 1 } } });
-db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
